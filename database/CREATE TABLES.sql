6403cee28e05ec3639c069beb26d59e40d37d55c
Sylicium <96630108+Sylicium@users.noreply.github.com>
2023-10-28 22:00:47 +0200

    Update CREATE TABLES.sql

--- database/CREATE TABLES.sql
+++ database/CREATE TABLES.sql
@@ -1,15 +1,33 @@
--- donjon.users definition
-
-CREATE TABLE `users` (
-  `id` varchar(100) DEFAULT NULL,
-  `discord_id` varchar(100) DEFAULT NULL,
-  `discord_name` varchar(100) DEFAULT NULL
-);
-
-
 -- donjon.levels definition
 
 CREATE TABLE `levels` (
-  `discord_id` varchar(100) DEFAULT NULL,
-  `xp` bigint(20) unsigned DEFAULT NULL
+  `discord_id` varchar(100) NOT NULL,
+  `xp` bigint(20) unsigned DEFAULT NULL,
+  PRIMARY KEY (`discord_id`)
+);
+
+
+-- donjon.users definition
+
+CREATE TABLE `users` (
+  `user_id` varchar(100) NOT NULL,
+  `guild_id` varchar(100) NOT NULL,
+  `isMember` tinyint(1) DEFAULT 0,
+  PRIMARY KEY (`user_id`,`guild_id`)
+);
+
+
+-- donjon.user_stats definition
+
+CREATE TABLE `user_stats` (
+  `user_id` varchar(100) NOT NULL,
+  `guild_id` varchar(100) NOT NULL,
+  `xp` bigint(20) unsigned DEFAULT 0,
+  `messages` int(10) unsigned DEFAULT 0,
+  `minutesInVoice` int(10) unsigned DEFAULT 0,
+  `adminGive` bigint(20) unsigned DEFAULT 0,
+  `react` bigint(20) unsigned DEFAULT 0,
+  `img` int(10) unsigned DEFAULT 0,
+  `level` int(10) unsigned DEFAULT 0,
+  PRIMARY KEY (`user_id`,`guild_id`)
 );
