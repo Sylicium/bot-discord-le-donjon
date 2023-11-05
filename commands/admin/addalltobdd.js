22ccbaf40c511c8b541113028c1c57672ff943e4
Sylicium <96630108+Sylicium@users.noreply.github.com>
2023-11-05 20:50:00 +0100

    Create addalltobdd.js

---
+++ commands/admin/addalltobdd.js
@@ -0,0 +1,48 @@
+const {
+  ActionRowBuilder,
+  ApplicationCommandType,
+  ButtonBuilder,
+  EmbedBuilder,
+  ButtonStyle
+} = require("discord.js")
+
+const {
+  roles
+} = require('../../config');
+
+module.exports = {
+  name: "addalltobdd",
+  type: ApplicationCommandType.ChatInput,
+  description: "Ajoute tous les membres à la base de données",
+  // default_member_permissions: ['Administrator'],
+  async run(client, interaction) {
+
+    let members = interaction.guild.members.cache.filter(x => {
+      return x._roles.includes(client.config.static.roles.membre)
+    }).map(x => x.id)
+
+
+    let ok=0;
+    let notok=0;
+    let total=0;
+    let failedList = []
+
+    for(let i in members) {
+      let member = members[i]
+      try {
+        client.db.initMember(member)
+        ok++
+      } catch(e) {
+        notok++
+        failedList.push(member)
+        console.log(e)
+      }
+      total++
+    }
+
+    interaction.reply({
+      content: `Opération terminée. ${ok}/${total} OK. ${notok} failed.\nFailed ID list: \`${failedList.join("\n")}\``
+    })
+
+  }
+}
