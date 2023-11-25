52818ad77875f86ed00e6912a2fe3cb19ec3f316
Sylicium <96630108+Sylicium@users.noreply.github.com>
2023-11-25 18:49:07 +0100

    /help fully functionnal

--- commands/tools/help.js
+++ commands/tools/help.js
@@ -4,50 +4,108 @@ const {
 } = Discord;
 const canvasCmd = require('../../functions/canvasCmd');
 const fs = require("fs")
+const somef = require("../../someFunctions")
 
 module.exports = {
     name: "help",
     description: "Affiche la liste des commandes",
     type: ApplicationCommandType.ChatInput,
+    options: [{
+      name: 'command',
+      description: 'Commande',
+      type: Discord.ApplicationCommandOptionType.String
+    }],
     run: async (client, interaction) => {
 
+        let commandeName = interaction.options.getString("command")?.toLowerCase().trim() || null
+        let commandes = client.commands.map(x => (x?.name?.toLowerCase().trim() ?? `${Date.now()}Error`))
 
+        function getCommandStringFormat(cmd) {
+            console.log("cmd.options:",cmd)
+            return `${cmd.name.trim()}${(cmd.options != null && cmd.options.length > 0) ? ` ${
+                cmd.options.map(y => {
+                    return `${y.required ? '<':'['}${y.name?.trim()}${y.required ? '>':']'}`
+                }).join(" ")}`: ""}`
+        }
+        
         function getEmojiPerm(command) {
-            console.log("command:",command) //command.hasOwnProperty(command.default_member_permissions), command.default_member_permissions?.length ?? "<0>")
-            console.log("property ->",command.hasOwnProperty(command.default_member_permissions)) //, command.default_member_permissions?.length ?? "<0>")
-            console.log("1:",command.default_member_permissions)
-            console.log("2:",(command.default_member_permissions.map(x => { return interaction.user.permissions.has(x) })))
+            //console.log("command:",command) //command.hasOwnProperty(command.default_member_permissions), command.default_member_permissions?.length ?? "<0>")
+            //console.log("property ->",command.hasOwnProperty("default_member_permissions")) //, command.default_member_permissions?.length ?? "<0>")
+            //console.log("1:",command.default_member_permissions)
+            //console.log("2:",(command.default_member_permissions?.map(x => { return interaction.member.permissions.has(x) })))
             return (
-                command.hasOwnProperty(command.default_member_permissions) && command.default_member_permissions.length > 0
+                command.hasOwnProperty("default_member_permissions") && command.default_member_permissions.length > 0
                 ? (
-                    !(command.default_member_permissions.map(x => { return interaction.user.permissions.has(x) })).includes(false)
+                    !(command.default_member_permissions.map(x => { return interaction.member.permissions.has(x) })).includes(false)
                     ? `🔑`
                     : "🔒" // "🔐"
                 ) : ""
             )
         }
 
-
-        let command_list = client.commands.map(x => {
-            return `${getEmojiPerm(x)} \`${x.name.trim()}${x.options ? ` ${
-                x.options.map(y => {
-                return `${y.required ? '<':'['}${y.name.trim()}${y.required ? '>':']'}`
-                }).join(" ")}`: ""}\`: _${x.description.trim()}_`
-        })
-
-
-        interaction.reply({
+        if(commandeName != null) {
+            let la_commande = client.commands.find(x => { return x.name?.toLowerCase().trim() == commandeName })
+            if(!la_commande) {
+                let guess = commandes.map(x => {
+                    return {
+                        cmd: x,
+                        score: somef.compareString(x, commandeName)
+                    }
+                }).sort((a,b) => b.score-a.score)
+                console.log("guess:",)
+                return interaction.reply({
+                    content: `Commande inconnue: \`${commandeName}\`. Vouliez vous écrire \`${guess[0].cmd}\` ?`,
+                    ephemeral: true
+                })
+            }
+            console.log("la_commande:",la_commande)
+            
+            interaction.reply({
                 embeds: [
                     new Discord.EmbedBuilder()
-                        .setTitle(`Liste des commandes du bot`)
+                        .setTitle(`${getEmojiPerm(la_commande)} /${la_commande.name}`)
+                        .setColor("00FF00")
                         .setDescription([
-                            `${command_list.join("\n")}`,
-                            ``,
-                            `🔑 Requiert des permissions que vous avez`,
-                            `🔒 Requiert des permissions d'administration`,
+                            `**Description:** _${la_commande.description}_`,
+                            `**Permissions:** ${la_commande.default_member_permissions.map(x => x.toUpperCase()).join(", ")}`,
+                            `**Format:** \`\`\`/${getCommandStringFormat(la_commande)}\`\`\``,
                         ].join("\n"))
-                ]
-        })
+                ],
+                ephemeral: true
+            })
+
+
+        } else {
+            
+            function haveperms(cmd) { return cmd.hasOwnProperty("default_member_permissions") && cmd.default_member_permissions.length > 0 }
+    
+            let command_list = client.commands.map(x => {
+                return `${getEmojiPerm(x)} \`${getCommandStringFormat(x)}\`: _${x.description.trim()}_`
+            }).sort((a,b) => {
+                if(haveperms(a) && !haveperms(b)) {
+                    return 1
+                } else if(!haveperms(a) && haveperms(b)) {
+                    return -1
+                }
+                return a.localeCompare(b)
+            })
+    
+    
+            interaction.reply({
+                    embeds: [
+                        new Discord.EmbedBuilder()
+                            .setTitle(`Liste des commandes du bot`)
+                            .setDescription([
+                                `${command_list.join("\n")}`,
+                                ``,
+                                `🔑 Requiert des permissions que vous avez`,
+                                `🔒 Requiert des permissions d'administration`,
+                            ].join("\n"))
+                            .setFooter({ text: `Faites /help [commande] pour avoir plus d'infos sur une commande.` })
+                    ]
+            })
+        
+        }
 
     }
 }
